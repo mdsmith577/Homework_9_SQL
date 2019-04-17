4edecbb77f0bfbcf40b220030c57698e035cae4f
mdsmith577 <46982383+mdsmith577@users.noreply.github.com>
2019-04-17 01:06:21 -0500

    Updated

--- Homework 9.sql
+++ Homework 9.sql
@@ -175,10 +175,21 @@ ORDER BY COUNT(*) DESC
 ;
 
 -- 7F
-
+SELECT staff.store_id AS "Store ID", SUM(payment.amount) AS "Sales per Store"
+FROM payment
+INNER JOIN staff ON payment.staff_id = staff.staff_id
+GROUP BY staff.store_id
+ORDER BY SUM(payment.amount) DESC
+;
 
 -- 7G
-
+SELECT store.store_id AS "Store ID", city.city AS "City", country.country AS "Country"
+FROM store
+INNER JOIN address ON store.address_id = address.address_id
+INNER JOIN city ON address.city_id = city.city_id
+INNER JOIN country ON city.country_id = country.country_id
+GROUP BY store.store_id
+;
 
 -- 7H
 
